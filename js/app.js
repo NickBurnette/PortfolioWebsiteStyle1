b4d51cd7505c05d934f364a314edae8231da4fa1
NickBurnette <burnette.nick@gmail.com>
2019-03-21 13:22:04 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
-
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-
-/* Otherwise just put the config content (json): */
-
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); *//*
-particlesJS.load('particles-js', 'particlesjs-config.json', function() {
-  console.log('callback - particlesjs-config.js config loaded');
-});*/
-
 particlesJS('particles-js',
   
   {
